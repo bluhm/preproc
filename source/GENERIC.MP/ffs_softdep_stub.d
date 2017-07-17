980165e812cfc91a5ab68ecd8e31bb9484250b11
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-07-17 22:56:34 +0200

    Do not commit the .d files.

--- source/GENERIC.MP/ffs_softdep_stub.d
+++
@@ -1,2 +0,0 @@
-ffs_softdep_stub.o:  \
- /home/bluhm/github/preproc/openbsd/src/sys/arch/sparc64/compile/GENERIC.MP/obj/../../../../../ufs/ffs/ffs_softdep_stub.c
--- source/GENERIC.MP/subr_tree.d
+++
@@ -1,8 +0,0 @@
-subr_tree.o:  \
- /home/bluhm/github/preproc/openbsd/src/sys/arch/sparc64/compile/GENERIC.MP/obj/../../../../../kern/subr_tree.c \
-  /home/bluhm/github/preproc/openbsd/src/sys/arch/sparc64/compile/GENERIC.MP/obj/../../../../../sys/tree.h \
-  /home/bluhm/github/preproc/openbsd/src/sys/arch/sparc64/compile/GENERIC.MP/obj/../../../../../sys/_null.h
-
-/home/bluhm/github/preproc/openbsd/src/sys/arch/sparc64/compile/GENERIC.MP/obj/../../../../../sys/tree.h:
-
-/home/bluhm/github/preproc/openbsd/src/sys/arch/sparc64/compile/GENERIC.MP/obj/../../../../../sys/_null.h:
